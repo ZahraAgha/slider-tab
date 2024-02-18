2968e6c00bd82423335ec463e7ee0b0aed045acb
ZahraAgha <zahrasha@code.edu.az>
2024-02-19 03:14:57 +0400

    commit

---
+++ script.js
@@ -0,0 +1,81 @@
+//slideCodes
+
+const slides = document.querySelector(".slides")
+const slide = document.querySelectorAll(".slide")
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+
+let currentIndex = 0
+let interval;
+
+function startAutoplay() {
+    interval = setInterval(() => {
+        if (currentIndex < slide.length - 1) {
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+        updateSlider();
+    }, 3000); // Adjust the interval as needed (in milliseconds)
+}
+
+function stopAutoplay() {
+    clearInterval(interval);
+}
+
+next.addEventListener("click", () => {
+    if (currentIndex < slide.length - 1) {
+        currentIndex++
+    } else {
+        currentIndex = 0
+    }
+    updateSlider()
+})
+
+prev.addEventListener("click", () => {
+    if (currentIndex > 0) {
+        currentIndex--
+    } else {
+        currentIndex = slide.length - 1
+    }
+    updateSlider()
+})
+
+slides.addEventListener("mouseenter", stopAutoplay);
+slides.addEventListener("mouseleave", startAutoplay);
+
+function updateSlider() {
+    const transformValue = -currentIndex * 100 + "%"
+    slides.style.transform = `translateX(${transformValue})`
+
+}
+
+
+startAutoplay();
+
+
+//tabCodes
+function showTab(tabId) {
+    const allContent = document.querySelectorAll(".content")
+    allContent.forEach(tab => {
+        tab.style.display = "none"
+    })
+
+    const selectedTab = document.getElementById(tabId)
+    selectedTab.style.display = "block"
+}
+
+// const allContent = document.querySelectorAll(".content");
+
+document.getElementById("dinner").addEventListener("click", () => {
+    showTab("content")
+})
+document.getElementById("breakfast").addEventListener("click", () => {
+    showTab("content")
+})
+document.getElementById("lunch").addEventListener("click", () => {
+    showTab("content")
+})
+
+
+
